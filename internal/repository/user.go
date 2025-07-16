932509ea3158612e4c72ac8dd8656d5ae7ecf423
BelovVA <bva.0337@gmail.com>
2025-07-16 13:01:41 +0300

    repos mutex added

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -1,17 +1,21 @@
 package repository
 
 import (
+	"sync"
+
 	"github.com/google/uuid"
 	"micro-blog/internal/model"
 )
 
 type UserRepo struct {
 	Users map[string]*model.User
+	mu    sync.RWMutex
 }
 
 func NewUserRepo() *UserRepo {
 	return &UserRepo{
 		Users: make(map[string]*model.User),
+		mu:    sync.RWMutex{},
 	}
 }
 
@@ -22,19 +26,27 @@ func (r *UserRepo) CreateUser(user *model.User) (*model.User, error) {
 	}
 
 	user.ID = id
+
+	r.mu.Lock()
+	defer r.mu.Unlock()
 	r.Users[user.Name] = user
 
 	return user, nil
 }
 
 func (r *UserRepo) GetUserByName(name string) (*model.User, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
 	if val, ok := r.Users[name]; ok {
 		return val, nil
 	}
+
 	return nil, model.ErrUserNotFound
 }
 
 func (r *UserRepo) GetUserById(id uuid.UUID) (*model.User, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
 	for _, val := range r.Users {
 		if val.ID == id {
 			return val, nil
